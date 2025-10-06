e5d124a8f33d3ed90570f7a3b40d931e1f5b65ba
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-10-06 16:11:15 +0330

    fixed loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const loading:React.FC = ()=>{
   return (
     <div className="flex h-screen items-center justify-center">
-        <div className="animate-ping rounded-full border-b border-gray-light">
+        <div className="animate-ping duration-500 ease-in-out rounded-full border-b border-gray-light">
             <p className="flex font-robotoBlack font-black text-7xl text-primary">Loading...</p>
         </div>
     </div>
