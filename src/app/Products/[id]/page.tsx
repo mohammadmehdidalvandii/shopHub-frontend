8e18c2c3eb4c0a9f4aa7389e7b373e9d7dbad5af
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-10-07 15:03:36 +0330

    created products/:id

---
+++ src/app/Products/[id]/page.tsx
@@ -0,0 +1,15 @@
+import Footer from '@/components/modules/Footer/Footer';
+import Navigation from '@/components/modules/Navigation/Navigation';
+import React from 'react';
+
+const page:React.FC = ()=>{
+  return (
+    <div className="min-h-screen bg-white">
+        <Navigation/>
+        this is products Details
+        <Footer/>
+    </div>
+  )
+}
+
+export default page
