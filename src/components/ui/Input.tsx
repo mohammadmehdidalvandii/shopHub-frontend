9f66f17cadf28cd95fc587e67e5844bf75326044
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-10-07 11:02:24 +0330

    fixed input and textaera mt-2

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -5,7 +5,7 @@ const Input = React.forwardRef<HTMLInputElement,React.ComponentProps<'input'>>((
     return(
         <input
         type={type}
-        className={cn('flex h-10 w-full rounded-md border border-input bg-white px-3 py-2 text-base ring-offset-primary file:bg-transparent file::text-sm file:font-robotoMedium font-semibold file:text-gray-medium placeholder:text-gray-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-border focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',className)}
+        className={cn('flex h-10 w-full rounded-md border mt-2 border-input bg-white px-3 py-2 text-base ring-offset-primary file:bg-transparent file::text-sm file:font-robotoMedium font-semibold file:text-gray-medium placeholder:text-gray-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-border focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',className)}
         ref={ref}
         {...props}
         />
--- src/components/ui/Textarea.tsx
+++ src/components/ui/Textarea.tsx
@@ -7,7 +7,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(({classNam
     return(
         <textarea 
         className={cn(
-            'flex min-h-[80px] w-full rounded-md border border-input bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-gray-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+            'flex min-h-[80px] w-full mt-2 rounded-md border border-input bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-gray-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
             className
         )}
         {...props}
