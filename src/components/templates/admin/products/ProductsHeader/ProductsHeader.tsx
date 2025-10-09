ae7f8fd296285776d39ba6b935c92fe6870b81c0
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-10-09 09:56:23 +0330

    fixed productsHeader text

--- src/components/templates/admin/products/ProductsHeader/ProductsHeader.tsx
+++ src/components/templates/admin/products/ProductsHeader/ProductsHeader.tsx
@@ -8,10 +8,10 @@ const ProductsHeader: React.FC = () => {
       <div className="flex flex-col sm:flex-row item-start sm:items-center justify-between gap-4 mb-8">
         <div>
           <h1 className="text-4xl font-robotoBlack font-black mb-2">
-            Admin Dashboard
+           Product Management
           </h1>
           <p className="text-xl text-gray-medium">
-            Welcome back ! Here's what's happening with your store
+            Manage your product inventory
           </p>
         </div>
         <Button variant="default" size="lg">
