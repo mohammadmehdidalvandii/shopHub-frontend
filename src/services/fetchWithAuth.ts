f96f055b454f9b8b07f951d8bbbcecfc1f5161a4
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-10-26 16:08:13 +0330

    created fetchWithAuth

---
+++ src/services/fetchWithAuth.ts
@@ -0,0 +1,17 @@
+import { getValidToken } from "./auth";
+
+export const fetchWithAuth = async (url:string, options:RequestInit = {})=>{
+    const token = getValidToken();
+    if(!token){
+        window.location.replace('/Auth')
+    };
+
+    return fetch(url,{
+        ...options,
+        headers:{
+            ...options.headers,
+            Authorization: `Bearer ${token}`,
+        },
+        credentials:'include',
+    })
+}
