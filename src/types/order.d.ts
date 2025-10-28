ae93e5e1a7c0f3c2125d0540f18a900bacc58c35
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-10-28 15:53:56 +0330

    created type-orders

---
+++ src/types/order.d.ts
@@ -0,0 +1,25 @@
+ export type Order = {
+  _id: string;
+  user: string;
+  items: {
+    product: string;
+    quantity: number;
+    price: number;
+  }[];
+  customerInfo: {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    address: string;
+    city: string;
+    state: string;
+    postalCode: string;
+    country: string;
+    orderNote?: string;
+  };
+  totalAmount: string;
+  status: 'pending' | 'paid' | 'shipped' | 'completed' | 'cancelled';
+  paymentMethod: string;
+  createdAt: string;
+};
