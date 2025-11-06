95bc22b9e800cd583fd7a135cbbb74e19c1b622e
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-11-06 14:52:15 +0330

    show product 3 for adminDashaborad

--- src/components/templates/admin/dashboard/DashboardProducts/DashboardProducts.tsx
+++ src/components/templates/admin/dashboard/DashboardProducts/DashboardProducts.tsx
@@ -1,10 +1,19 @@
+"use client"
 import { Button } from '@/components/ui/Button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/Cart';
+import { useGetAllProducts } from '@/services/productServices';
 import { ArrowUpRight } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 
 const DashboardProducts:React.FC = ()=>{
+       const {data , isError , isLoading} = useGetAllProducts();    
+           if(isLoading){
+            return <p>Loading...</p>
+        };
+         if(isError){
+            return <p>Failed to load orders</p>
+        };
   return (
     <div className="container mx-auto">
         <Card>
@@ -19,48 +28,22 @@ const DashboardProducts:React.FC = ()=>{
             </CardHeader>
             <CardContent>
                 <div className="space-y-4">
-                    <div className="flex items-center justify-between p-4 rounded-lg border border-border">
+                    {data.slice(0,3).map((product:any)=>(
+                    <div className="flex items-center justify-between p-4 rounded-lg border border-border" key={product._id}>
                         <div className="flex items-center gap-4">
                             <div className="w-8 h-8 rounded-full bg-primary/10 flex items-center justify-center">
-                                <span className="font-bold text-primary">#1</span>
+                                <span className="font-bold text-primary">{product.productName[0]}</span>
                             </div>
                             <div>
-                                <p className="font-semibold">product name</p>
-                                <p className="text-lg text-gray-medium">245 sales</p>
+                                <p className="font-semibold">{product.productName}</p>
+                                <p className="text-lg text-gray-medium">{product.stockQuantity} items</p>
                             </div>
                         </div>
                         <div className="text-right">
-                            <p className="font-roboto-bold text-primary">$7,876.87</p>
+                            <p className="font-roboto-bold text-primary">${product.price}</p>
                         </div>
                     </div>
-                    <div className="flex items-center justify-between p-4 rounded-lg border border-border">
-                        <div className="flex items-center gap-4">
-                            <div className="w-8 h-8 rounded-full bg-primary/10 flex items-center justify-center">
-                                <span className="font-bold text-primary">#1</span>
-                            </div>
-                            <div>
-                                <p className="font-semibold">product name</p>
-                                <p className="text-lg text-gray-medium">245 sales</p>
-                            </div>
-                        </div>
-                        <div className="text-right">
-                            <p className="font-roboto-bold text-primary">$7,876.87</p>
-                        </div>
-                    </div>
-                    <div className="flex items-center justify-between p-4 rounded-lg border border-border">
-                        <div className="flex items-center gap-4">
-                            <div className="w-8 h-8 rounded-full bg-primary/10 flex items-center justify-center">
-                                <span className="font-bold text-primary">#1</span>
-                            </div>
-                            <div>
-                                <p className="font-semibold">product name</p>
-                                <p className="text-lg text-gray-medium">245 sales</p>
-                            </div>
-                        </div>
-                        <div className="text-right">
-                            <p className="font-roboto-bold text-primary">$7,876.87</p>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </CardContent>
         </Card>
