a2f72656fa3a4978ecebc4057944118288debbf8
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-11-08 16:20:59 +0330

    get All wishlist by UserId

--- src/components/templates/profile/ProfileWishlist/ProfileWishlist.tsx
+++ src/components/templates/profile/ProfileWishlist/ProfileWishlist.tsx
@@ -1,41 +1,58 @@
+"use client"
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/Cart';
 import { Heart, Trash2 } from 'lucide-react';
 import Image from 'next/image';
 import React from 'react';
-import Product from '../../../../../public/assets/images/product.png'
 import { Button } from '@/components/ui/Button';
+import { useGetAllWishlistUserId } from '@/services/wishlistServices';
 
 const ProfileWishlist:React.FC = ()=>{
+
+    const {data , isError , isLoading} = useGetAllWishlistUserId();
+        if(isLoading){
+        return <p>Loading...</p>
+    };
+
+    if(isError){
+        return <p>Failed to load Wishlist 😢</p>
+    };
+
+    if(data.length === 0){
+        return (
+             <div className="text-center py-12">
+                <Heart className='h-16 w-16 mx-auto text-gray-medium mb-4'/>
+                <p className="text-xl text-gray-medium">Your wishlist is empty</p>
+                <p className="text-lg text-gray-medium mt-2">Add products you love to keep track of them</p>
+            </div> 
+        )
+    }
+
+
   return (
     <Card className='max-h-[60vh] overflow-y-auto'>
         <CardHeader>
             <CardTitle>My Wishlist</CardTitle>
         </CardHeader>
-        <CardContent>
-            {/* Wishlist empty */}     
-            {/* <div className="text-center py-12">
-                <Heart className='h-16 w-16 mx-auto text-gray-medium mb-4'/>
-                <p className="text-xl text-gray-medium">Your wishlist is empty</p>
-                <p className="text-lg text-gray-medium mt-2">Add products you love to keep track of them</p>
-            </div> */}
-
+        <CardContent>   
             <div className="grid gap-4 md:grid-cols-2">
                 <Card className='p-2'>
                     <CardContent>
-                        <div className="flex flex-col md:flex-row gap-4">
+                        {data.items.map((item:any)=>(
+                            <div className="flex flex-col md:flex-row gap-4" key={item.product._id}>
                             <div className="w-24 h-24 rounded-lg overflow-hidden bg-gray-light">
-                                <Image src={Product} alt='products' width={200} height={200} className='w-full h-full '/>
+                                <Image src={item.product.images[0]} alt='products' width={200} height={200} className='w-full h-full '/>
                             </div>
                             <div className="flex-1 min-w-0">
-                                <h3 className="font-robotoBold font-semibold text-xl mb-1 truncate">product name</h3>
-                                <p className="text-lg text-gray-medium">category</p>
-                                <p className="text-2xl font-robotoBold font-bold text-primary mb-3">$899.99</p>
+                                <h3 className="font-robotoBold font-semibold text-xl mb-1 truncate">{item.product.productName}</h3>
+                                <p className="text-lg text-gray-medium">{item.product.category.title}</p>
+                                <p className="text-2xl font-robotoBold font-bold text-primary mb-3">${item.product.price}</p>
                                 <div className="flex gap-2">
                                     <Button variant='accent' size='sm' className='flex-1'>Add to Cart</Button>
                                     <Button variant='outline' size='sm'><Trash2 className='h-4 w-4'/></Button>
                                 </div>
                             </div>
                         </div>
+                        ))}
                     </CardContent>
                 </Card>
 
--- src/services/wishlistServices.ts
+++ src/services/wishlistServices.ts
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { fetchWithAuth } from "./fetchWithAuth";
 import { useAuthStore } from "@/store/authStore";
 
@@ -21,5 +21,28 @@ export  const useAddToWishlist = ()=>{
             const data = await res.json();
             return data.data;
         }
+    })
+};
+
+export const useGetAllWishlistUserId = ()=>{
+     const {user} = useAuthStore.getState()
+    return useQuery({
+        queryKey:['wishlist'],
+        queryFn:async ()=>{
+            const res = await fetch(`${API_URL}${user?._id}`,{
+                method:'GET'
+            });
+            if(!res.ok){
+                const errorData =  await res.json();
+                throw new Error(errorData.message || 'get wishlist userID failed');
+            };
+
+            const data = await res.json();
+            return data.data
+        },
+        enabled:!!user?._id,
+        retry:2,
+        staleTime: 1000 * 60 * 60 * 24,
+        refetchOnWindowFocus:false,
     })
 }
